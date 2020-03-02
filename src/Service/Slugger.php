9f6307ffa34bca6bb262e02bd27116704db6054d
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-03-02 10:22:16 +0100

    1.2 : add Service to slugify string

---
+++ src/Service/Slugger.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service;
+
+class Slugger
+{
+    public function slugify(string $stringToSlug): string
+    {
+        return preg_replace( '/[^a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*/', '-', strtolower(trim(strip_tags($stringToSlug))) );
+    }
+}
