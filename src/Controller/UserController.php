49292e17d5b8705e4aff0d1bf316c54778a97720
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-03-02 17:32:45 +0100

    1.2 : add UserController's method -> create

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,8 +6,11 @@ use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 /**
  * @Route("/users", name="api_")
@@ -17,15 +20,15 @@ class UserController extends AbstractController
     /**
      * Retrieves all users
      *
-     * @Route(name="users_list", methods={"GET"})
-     *
      * @param UserRepository $userRepository
      * 
      * @return JsonResponse
+     *
+     *  @Route(name="users_list", methods={"GET"})
      */
     public function getAll(UserRepository $userRepository): JsonResponse
     {
-        $users = $userRepository->findall();
+        $users = $userRepository->findAll();
 
         return $this->json(
             $users,
@@ -38,10 +41,11 @@ class UserController extends AbstractController
     /**
      * Retrieves a partical user
      *
-     * @Routes("/{slug}", name="user", methods={"GET"})
-     * 
      * @param User $user
+     * 
      * @return JsonResponse
+     * 
+     * @Route("/{slug}", name="user", methods={"GET"})
      */
     public function getOne(User $user = null): JsonResponse
     {
@@ -57,6 +61,75 @@ class UserController extends AbstractController
             Response::HTTP_OK,
             array(),
             ['groups' => 'users_list']
+        );
+    }
+
+    /**
+     * Create a new user
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param ValidatorInterface $validator
+     * 
+     * @return JsonResponse
+     * 
+     * @Route(name="create_user", methods={"POST"})
+     */
+    public function create(
+        Request $request,
+        SerializerInterface $serializer,
+        ValidatorInterface $validator
+    ) {
+
+        //TODO make voters
+
+        $content = $request->getContent();
+
+        if (json_decode($content) === null) {
+            return $this->json(
+                ['error' => 'invalid data format'],
+                Response::HTTP_UNAUTHORIZED
+            );
+        }
+
+        $user = $serializer->deserialize(
+            $content,
+            User::class,
+            'json',
+            ['groups' => 'user_create']
+        );
+
+        $errors = $validator->validate($user);
+
+        if (count($errors) !== 0) {
+            $errorsList = array();
+
+            foreach ($errors as $error) {
+                $errorsList[] = [
+                    'field'     => $error->getPropertyPath(),
+                    'message'   => $error->getMessage()
+                ];
+            }
+
+            return $this->json(
+                $errorsList,
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        $manager = $this
+            ->getDoctrine()
+            ->getManager();
+
+        $manager->persist($user);
+        $manager->flush();
+
+        return $this->json(
+            [
+                'message' => 'user created',
+                'content' => $user
+            ],
+            Response::HTTP_CREATED
         );
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -16,27 +17,32 @@ class User implements UserInterface
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     * @Groups({"celestial-body"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=30, unique=true)
+     * @Groups({"celestial-body", "user_create"})
      */
     private $nickname;
 
     /**
      * @ORM\Column(type="string", length=30)
+     * @Groups({"celestial-body"})
      */
     private $slug;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"user_create"})
      */
     private $email;
 
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups({"user_create"})
      */
     private $password;
 
@@ -48,6 +54,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=50, nullable=true)
+     * @Groups({"celestial-body"})
      */
     private $avatar;
 
@@ -84,6 +91,7 @@ class User implements UserInterface
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\Rank", inversedBy="users")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"celestial-body"})
      */
     private $rank;
 
