a0716f0938f22bd956f68dfa3019b17bd176d2a8
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-03-10 18:17:51 +0100

    s2.1 : UserEvent done

--- src/EventListener/UserEvent.php
+++ src/EventListener/UserEvent.php
@@ -5,25 +5,61 @@ namespace App\EventListener;
 use App\Entity\User;
 use App\Repository\RankRepository;
 use App\Repository\RoleRepository;
+use App\Service\Slugger;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserEvent
 {
     private $rankRepository;
     private $roleRepository;
+    private $slugger;
+    private $encoder;
 
-    public function __construct(RankRepository $rankRepository, RoleRepository $roleRepository)
+    public function __construct(
+        RankRepository $rankRepository,
+        RoleRepository $roleRepository,
+        Slugger $slugger,
+        UserPasswordEncoderInterface $encoder
+    )
     {
         $this->rankRepository = $rankRepository;
         $this->roleRepository = $roleRepository;
+        $this->slugger = $slugger; 
+        $this->encoder = $encoder;
     }
 
     public function prePersist(User $user, LifecycleEventArgs $event)
     {
-        $rank = $this->rankRepository->findOneByName('neophyte');
+        $rank = $this->rankRepository->findOneByRankNumber(1);
         $user->setRank($rank);
 
         $role = $this->roleRepository->findOneByName('ROLE_CONTRIBUTOR');
         $user->setRole($role);
+
+        $user->setPassword(
+            $this->encoder->encodePassword(
+                $user,
+                $user->getPassword()
+            )
+        );
+
+        $user->setSlug(
+            $this->slugger->slugify($user->getNickname())
+        );
     }
+
+    public function preUpdate(User $user, LifecycleEventArgs $event)
+    {
+        $user->setPassword(
+            $this->encoder->encodePassword(
+                $user,
+                $user->getPassword()
+            )
+        );
+
+        $user->setSlug(
+            $this->slugger->slugify($user->getNickname())
+        );
+    }    
 }
