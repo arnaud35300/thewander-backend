6dbedbb65d0c5a0d4d1dea872fd6d8a60a180560
WilliamMgy <william.marigny@gmail.com>
2020-03-11 12:09:12 +0100

    S2.2 : constructor created

--- src/Service/Delimiter.php
+++ src/Service/Delimiter.php
@@ -2,10 +2,24 @@
 
 namespace App\Service;
 
+use App\Repository\CelestialBodyRepository;
+
 class Delimiter
 {
+    private $celestialBodyRepository;
+
+    public function __construct(CelestialBodyRepository $celestialBodyRepository)
+    {
+        $this->celestialBodyRepository = $celestialBodyRepository;
+    }
+
     public function verifyMargins()
     {
-        # ...
+        $celestialBodies = $this->celestialBodyRepository->findAll();
+
+        foreach ($celestialBodies as $celestialBody) {
+            $xPosition = $celestialBody->getXPosition();
+            $yPosition = $celestialBody->getYPosition();
+        }
     }
 }
