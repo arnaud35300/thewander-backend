25d3aa0462fc5e52d9713a8ccf5f0d8989e95bb8
WilliamMgy <william.marigny@gmail.com>
2020-03-11 14:13:12 +0100

    S2.2 : service created

--- src/Service/Delimiter.php
+++ src/Service/Delimiter.php
@@ -13,13 +13,23 @@ class Delimiter
         $this->celestialBodyRepository = $celestialBodyRepository;
     }
 
-    public function verifyMargins()
+    public function verifyMargins(int $newX, int $newY): bool
     {
         $celestialBodies = $this->celestialBodyRepository->findAll();
 
         foreach ($celestialBodies as $celestialBody) {
             $xPosition = $celestialBody->getXPosition();
             $yPosition = $celestialBody->getYPosition();
+
+            $minX = $xPosition;
+            $maxX = $xPosition + 200;
+            $minY = $yPosition;
+            $maxY = $yPosition + 200;
+
+            if (($newX >= $minX) || ($newX <= $maxX) && ($newY >= $minY) || ($newY <= $maxY)) 
+                return false;
         }
+
+        return true;
     }
 }
