791df344cf387e277dba72b83577c274c27824e6
arnaud35300 <arnaudguillardcontactpro@gmail.com>
2020-03-17 17:30:06 +0100

    S3.1 : Email in UserController

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
